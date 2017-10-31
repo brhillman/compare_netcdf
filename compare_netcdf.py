6dafdf45c9ea26a4538b28bfcef7377bd4a981f6
Benjamin Hillman <benjaminr.hillman@gmail.com>
2017-10-31 14:25:20 -0600

    Add script to repository
    Add compare_netcdf script to repository.

---
+++ compare_netcdf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+def main(file1, file2):
+    import numpy
+
+    # open datasets
+    from xarray import open_dataset
+    ds = [open_dataset(f, decode_times=False) for f in (file1, file2)]
+
+    # loop over dimensions and check for differences
+    for dname in ds[0].dims:
+        if ds[1].dims[dname] != ds[0].dims[dname]:
+            print('Dimension %s differs')
+
+    # loop over variables and check for differences
+    for vname in ds[0].variables.keys():
+        try:
+            difference = (ds[1].variables[vname].values - ds[0].variables[vname].values)
+            average = (ds[1].variables[vname].values + ds[0].variables[vname].values) / 2
+            fractional_difference = numpy.where(average > 0, difference / average, 0)
+            if difference.max() > 0:
+                print(
+                    'Variable %s differs (max difference: %e; %f%%)'%(
+                        vname, difference.max(), 100.0 * fractional_difference.max()
+                    )
+                )
+        except:
+            print('Processing for variable %s failed.'%vname)
+
+    # close datasets
+    for d in ds: d.close()
+
+
+if __name__ == '__main__':
+    import plac
+    plac.call(main)
